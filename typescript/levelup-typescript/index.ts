82399c4c653146d81620339f59debe2fe51b459c
Tanner <thetannerthomas@gmail.com>
2021-09-28 21:36:37 -0600

    remove comments

--- typescript/levelup-typescript/index.ts
+++ typescript/levelup-typescript/index.ts
@@ -1,16 +1,3 @@
-// interface Person {
-//   name: string;
-//   age?: number;
-// }
-
-// function print({ name, age }: Person): Person {
-//   console.log(name);
-//   return { name };
-// }
-
-// print({ name: "tanner", age: 55 });
-// // print({ age: 55, name: "mike" });
-
 interface Dog {
   name: string;
   age?: number;
