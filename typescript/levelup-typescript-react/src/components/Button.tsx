437646aed718b4c20dab8d7a4010fa296943ec10
Tanner <thetannerthomas@gmail.com>
2021-09-30 17:49:23 -0600

    event types

--- typescript/levelup-typescript-react/src/components/Button.tsx
+++ typescript/levelup-typescript-react/src/components/Button.tsx
@@ -1,9 +1,10 @@
-import { MouseEvent } from "react";
+import { MouseEvent, FormEvent } from "react";
 
 // #7 React Events in TypeScript
 
 interface Props {
   onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  onChange?: (e: FormEvent<HTMLInputElement>) => void;
 }
 
 export default function Button({ onClick }: Props) {
